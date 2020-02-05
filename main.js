f497029360e0272acaea61be2cb24f365edcf74a
tayronea <tayron_machado@hotmail.com>
2020-02-05 15:30:10 -0300

    Material Curso

---
+++ main.js
@@ -0,0 +1,57 @@
+import { EvalSourceMapDevToolPlugin } from "webpack";
+
+/*
+//percorre todo o array e aplica a determinada função 
+
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+
+// Consome o vetor e transforma em uma unica variável
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+//Aplica uma determinada regra, retorna true ou false
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+// Utilizado para verificar se existe alguma informaçao dentro do array 
+const find = arr.find(function(item){
+    return item === 33;
+});
+
+
+class usuario {
+    constructor(){
+        email = this.email;
+        senha = this.senha;
+        isAdmin = false;
+    } 
+    
+    isAdmin(){
+        if (this.usuario.isAdmin === true)
+            return true;
+        else
+            return false;    
+    }
+}
+
+
+class Admin extends Usuario {
+    constructor(){
+        super();
+        admin = true;
+    }
+}
+
+//const User1 = new Usuario('email@teste.com', 'senha123');
+//const Adm1 = new Admin('email@teste.com', 'senha123');
+
+
+//console.log(User1.isAdmin()) // false
+//console.log(Adm1.isAdmin()) // true
+*/
